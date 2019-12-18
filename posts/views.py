c430c5258bebf318c5f88d4100cc375fae1fe52c
Wilson Forero <wilson_camilo@hotmail.com>
2019-12-18 00:48:34 -0500

    Se añadió la función render para enviar los posts a nuestro html y se modificó la lista posts para que quede más presentable, pero esta es de prueba

--- posts/views.py
+++ posts/views.py
@@ -1,38 +1,42 @@
 # django
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # utilities
 from datetime import datetime
 # Solo para pruebas:
 posts = [
     {
-        'name': 'Chocolates',
-        'user': 'Wilson Forero',
+        'title': 'Chocolates',
+        'user': {
+            'name': 'Wilson Forero',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036'
+        'photo': 'https://picsum.photos/200/200/?image=1036'
     },
     {
-        'name': 'Via Lactea',
-        'user': 'Andrés A',
+        'title': 'Via Lactea',
+        'user': {
+            'name': 'Andrés A',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903'
+        'photo': 'https://picsum.photos/200/200/?image=903'
     },
     {
-        'name': 'Auditorio',
-        'user': 'Johan W',
+        'title': 'Auditorio',
+        'user': {
+            'name': 'Johan W',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076'
+        'photo': 'https://picsum.photos/200/200/?image=1076'
     }
 ]
 
 def list_posts(request):
     ''' List existing post '''
-    content = []
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <figure><img src={picture}/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {
+        'posts': posts
+    })
+
