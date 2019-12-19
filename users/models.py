5ef8b000b52717751a90bdcb430bf3823b36f106
Wilson Forero <wilson_camilo@hotmail.com>
2019-12-18 20:37:57 -0500

    Aplicación de usuarios incluído el modelo de Profile

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    """
+    El perfil hereda de model y tiene un User asociado
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True) 
+
+    picture = models.ImageField(
+        upload_to='users/pictures', 
+        blank=True, 
+        null=True
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    modify = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.username
