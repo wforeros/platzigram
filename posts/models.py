b13e4f984e48024a10a57146126ac1f9de09076f
Wilson Forero <wilson_camilo@hotmail.com>
2019-12-18 19:43:03 -0500

    Usuarios eliminados de la app posts (se creará la app users)

--- posts/models.py
+++ posts/models.py
@@ -1,25 +1,2 @@
 # django
 from django.db import models
-
-class User(models.Model):
-    """User test model"""
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    is_admin = models.BooleanField(default=False)
-
-    bio = models.TextField(blank=True)
-    birthdate = models.DateField(blank=True, null=True)
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
-    country = models.CharField(max_length=20, default='')
-    city = models.CharField(max_length=40, default='')
-
-    def __str__(self):
-        return self.email
-
