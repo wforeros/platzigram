0a7c5f41472b04120d1c8ecf5e9606e925516735
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-02 23:58:35 -0500

    Método para la vista de signup creado

--- users/views.py
+++ users/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render, redirect
 
+# Exceptions
+from django.db.utils import IntegrityError
+
+# Modelos
+from django.contrib.auth.models import User
+from users.models import Profile
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -26,3 +33,26 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirmation']
+        if password != password_confirm:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+        
+        try:
+            user = User.objects.create_user(username, password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already registered'})
+        
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
