19f01143e0e910d48432655469e214fb07ce20ee
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-02 22:24:35 -0500

    Vista de login añadida

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            # Redirige a la url con nombre feed (urls.py)
+            return redirect('feed')
+            
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, 'users/login.html', {'error': 'Invalid username or password'})
+
+        
+    return render(request, 'users/login.html')
