b5c1857e7a6931861b3f3fb6ef7d7d4821a2a10a
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-02 21:23:41 -0500

    Admin de los posts añadido

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+#Local imports
+from posts.models import Post
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'profile', 'created', 'modified')
+
+    search_fields = (
+        'title',
+        'profile__user__username'
+    )
+
+
