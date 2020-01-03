e821e6725df17767f49a146a9fc53ed86d80af67
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-02 21:25:24 -0500

    Propiedad de modificado añadida en el modelo Posts

--- posts/models.py
+++ posts/models.py
@@ -9,8 +9,8 @@ class Post(models.Model):
     profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='posts/photos')
-
     created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{} by {}'.format(self.title, self.user.username)
