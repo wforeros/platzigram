77a58e7f7975ae6fdcaa3356969e0ad42ccc40a8
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-04 21:59:06 -0500

    Incluída vista de creación de post y list_post actualizada

--- posts/views.py
+++ posts/views.py
@@ -1,44 +1,40 @@
 # django
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 # utilities
 from datetime import datetime
+
+from posts.forms import PostForm
+from posts.models import Post
 # Solo para pruebas:
 posts = [
-    {
-        'title': 'Chocolates',
-        'user': {
-            'name': 'Wilson Forero',
-            'picture': 'https://picsum.photos/60/60/?image=1027'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=1036'
-    },
-    {
-        'title': 'Via Lactea',
-        'user': {
-            'name': 'Andrés A',
-            'picture': 'https://picsum.photos/60/60/?image=1005'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=903'
-    },
-    {
-        'title': 'Auditorio',
-        'user': {
-            'name': 'Johan W',
-            'picture': 'https://picsum.photos/60/60/?image=883'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=1076'
-    }
 ]
 
 @login_required
 def list_posts(request):
     ''' List existing post '''
+    posts = Post.objects.all().order_by('-created')
     return render(request, 'posts/feed.html', {
         'posts': posts
     })
 
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+
+    return render(
+        request,
+        template_name='posts/new.html',
+        context={
+            'form': form, 
+            'user': request.user,
+            'profile': request.user.profile
+        }
+    )
