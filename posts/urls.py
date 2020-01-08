57d35174ab1e8d1f9b439ae1e62cf03ea702edce
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-08 00:12:03 -0500

    Urls relacionadas a posts

---
+++ posts/urls.py
@@ -0,0 +1,20 @@
+"""
+Posts urls
+"""
+# Django
+from django.urls import path
+from posts import views
+
+urlpatterns = [
+    path(
+        route='', 
+        view=views.list_posts, 
+        name='feed'
+    ),
+
+    path(
+        route='new/', 
+        view=views.create_post, 
+        name='create'
+    ),
+]
