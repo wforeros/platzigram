71e1ec20ccc8721728ca02be9f453b141f5dfe1c
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-08 01:05:13 -0500

    Creada la vista detail basada en clases

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
+from django.urls import reverse
+
 
 # Exceptions
 from django.db.utils import IntegrityError
@@ -10,10 +14,28 @@ from django.db.utils import IntegrityError
 # Modelos
 from django.contrib.auth.models import User
 from users.models import Profile
+from posts.models import Post
 
 # Forms
 from users.forms import ProfileForm
 from users.forms import SignupForm
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    
+    template_name='users/detail.html'
+    slug_field='username'
+    # Este nombre es el que está en la url, sale <str:username>
+    slug_url_kwarg='username'
+    queryset=User.objects.all()
+    context_object_name='user'
+
+    # Para agregar los posts sobre escribiremos el metodo get_context_data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['posts'] = Post.objects.filter(user=user).order_by('-created')
+        return context
 
 
 def login_view(request):
@@ -68,7 +90,8 @@ def update_profile(request):
             profile.picture = data['picture']
             profile.save()
             
-            return redirect('users:update_profile')
+            url = reverse('users:detail', kwargs={'username': request.user.username})
+            return redirect(url)
     else:
         form = ProfileForm()
     return render(request, 
