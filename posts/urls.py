658f9c5a56f1af2c4d52e360da1927b8cc0eebaa
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-08 21:14:24 -0500

    Url de detail modificada (recibe el id) y view de new-posts ahora basada en clases

--- posts/urls.py
+++ posts/urls.py
@@ -13,14 +13,14 @@ urlpatterns = [
     ),
 
     path(
-        route='post/detail/',
+        route='post-detail/<str:post_id>',
         view=views.PostDetailView.as_view(),
         name='detail'
     ),
 
     path(
         route='new-post/', 
-        view=views.create_post, 
+        view=views.CreatePostView.as_view(), 
         name='create'
     ),
 ]
