1935a0b503ca98480ac1f5a0a83159ba3c8599f0
Wilson Forero <wilson_camilo@hotmail.com>
2020-01-08 22:08:35 -0500

    Corrección de url de la forma app:url_name

--- platzigram/middleware.py
+++ platzigram/middleware.py
@@ -27,8 +27,8 @@ class ProfileCompletionMiddleware:
             if not request.user.is_staff:
                 profile = Profile.objects.get(user = request.user)
                 if not profile.picture or not profile.biography:
-                    if request.path not in [reverse('update_profile'), reverse('logout')]:
-                        return redirect('update_profile')
+                    if request.path not in [reverse('users:update_profile'), reverse('users:logout')]:
+                        return redirect('users:update_profile')
 
         response = self.get_response(request)
         return response
